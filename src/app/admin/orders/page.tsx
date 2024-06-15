2f048e00eb420bd0fad6d9330caebffb413e15cb
JwanCortez93 <jwancortez@gmail.com>
2024-06-15 18:50:14 -0300

    admin orders page init

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,81 @@
+import db from "@/db/db";
+import PageHeader from "../_components/PageHeader";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency } from "@/lib/formatters";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreVertical } from "lucide-react";
+import DeleteDropdownItem from "./_components/OrderActions";
+
+const getOrders = () => {
+  return db.order.findMany({
+    select: {
+      id: true,
+      pricePaidInCents: true,
+      product: { select: { name: true } },
+      user: { select: { email: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+};
+
+export default function OrdersPage() {
+  return (
+    <>
+      <PageHeader>Sales</PageHeader>
+      <OrdersTable />
+    </>
+  );
+}
+
+const OrdersTable = async () => {
+  const orders = await getOrders();
+
+  if (orders.length === 0) return <p>Not orders found</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Product</TableHead>
+          <TableHead>Customer</TableHead>
+          <TableHead>Price paid</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => (
+          <TableRow>
+            <TableCell>{order.product.name}</TableCell>
+            <TableCell>{order.user.email}</TableCell>
+            <TableCell>{formatCurrency(order.pricePaidInCents)}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className="sr-only">Actions</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DeleteDropdownItem id={order.id} />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
